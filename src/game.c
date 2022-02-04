396a8cba2aeaa74ebf9d9407996f3d3795062954
unknown <edeleb@comcast.net>
2022-02-04 00:09:04 -0500

    entity manager start

--- src/game.c
+++ src/game.c
@@ -3,6 +3,8 @@
 #include "gf2d_sprite.h"
 #include "simple_logger.h"
 
+#include "entity.h"
+
 int main(int argc, char * argv[])
 {
     /*variable declarations*/
@@ -62,7 +64,7 @@ int main(int argc, char * argv[])
         gf2d_grahics_next_frame();// render current draw frame and skip to the next frame
         
         if (keys[SDL_SCANCODE_ESCAPE])done = 1; // exit condition
-        slog("Rendering at %f FPS",gf2d_graphics_get_frames_per_second());
+        //slog("Rendering at %f FPS",gf2d_graphics_get_frames_per_second());
     }
     slog("---==== END ====---");
     return 0;
